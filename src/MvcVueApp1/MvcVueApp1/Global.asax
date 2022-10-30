3d26ee41745fbd1423800714b01dd3cd1a3ac93f
ggo <ggo@clickndecide.com>
2022-10-30 09:50:48 +0100

    1st cmmmit.

---
+++ src/MvcVueApp1/MvcVueApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcVueApp1.MvcApplication" Language="C#" %>
---
+++ src/MvcVueApp1/MvcVueApp1/Models/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI.WebControls;
+
+namespace MvcVueApp1.Models
+{
+    public class Helpers
+    {
+        public static IHtmlString LoadFile(string webpath)
+        {
+            string filePath = HostingEnvironment.MapPath(webpath);
+            IHtmlString ret = new HtmlString(File.ReadAllText(filePath));
+            return ret;
+        }
+    }
+}
